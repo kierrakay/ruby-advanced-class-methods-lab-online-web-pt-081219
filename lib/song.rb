f791dc91277eff51c28183bb4914c93b04c22729
Kierra M <k.merriman32@hotmail.com>
2019-09-19 23:31:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ end
 end
 
 def self.new_from_filename(name)
-  song = song.new
+  song = self.new
   song.name = (name.split("-")[1].chomp("mp3"))
   song.artist_name = (name.split("-")[0])
   song
